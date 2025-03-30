68c7d9b04cee6db037631ddd6a06e0ea47de0717
Pavel <76450479+carbonbes@users.noreply.github.com>
2025-03-30 13:34:49 +0300

    fix/date-picker

--- components/shared/DatePicker.vue
+++ components/shared/DatePicker.vue
@@ -12,7 +12,7 @@
     >
       <DatePickerArrow class="fill-white stroke-gray-300/50" />
 
-      <DatePickerCalendar v-slot="{ weekDays, grid }" class="p-4">
+      <DatePickerCalendar v-slot="{ weekDays, grid }" class="p-4 flex flex-col gap-4">
         <DatePickerHeader class="flex items-center justify-between">
           <DatePickerPrev asChild>
             <Flex tag="button" center class="p-1.5 rounded-full hover:bg-gray-200">
@@ -20,7 +20,7 @@
             </Flex>
           </DatePickerPrev>
 
-          <DatePickerHeading class="font-medium" />
+          <DatePickerHeading class="font-medium first-letter:uppercase" />
 
           <DatePickerNext asChild>
             <Flex tag="button" center class="p-1.5 rounded-full hover:bg-gray-200">
@@ -30,7 +30,7 @@
         </DatePickerHeader>
 
         <div
-          class="flex flex-col space-y-4 pt-4 sm:flex-row sm:space-x-4 sm:space-y-0"
+          class="flex flex-col space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0"
         >
           <DatePickerGrid
             v-for="month in grid"
@@ -42,7 +42,7 @@
                 <DatePickerHeadCell
                   v-for="day in weekDays"
                   :key="day"
-                  class="w-8 rounded-md text-xs text-green8"
+                  class="w-8 rounded-md text-xs"
                 >
                   {{ day }}
                 </DatePickerHeadCell>
@@ -60,16 +60,28 @@
                   :key="weekDate.toString()"
                   :date="weekDate"
                 >
-                  <DatePickerCellTrigger
-                    :day="weekDate"
-                    :month="month.value"
-                    class="relative flex items-center justify-center whitespace-nowrap rounded-full border-2 border-transparent bg-transparent text-sm font-normal text-black w-8 h-8 outline-none focus:shadow-[0_0_0_2px] focus:shadow-black hover:border-blue-500 data-[selected]:bg-blue-500 data-[selected]:font-medium data-[outside-view]:text-black/30 data-[selected]:text-white data-[unavailable]:pointer-events-none data-[unavailable]:text-black/30 data-[unavailable]:line-through before:absolute before:top-[5px] before:hidden before:rounded-full before:w-1 before:h-1 before:bg-white data-[today]:before:block data-[today]:before:bg-green9 data-[selected]:before:bg-white"
-                  />
+                  <DatePickerClose asChild>
+                    <DatePickerCellTrigger
+                      :day="weekDate"
+                      :month="month.value"
+                      class="relative flex items-center justify-center whitespace-nowrap rounded-full border-2 border-transparent bg-transparent text-sm font-normal text-black w-8 h-8 outline-none hover:border-blue-500 data-[selected]:bg-blue-500 data-[selected]:font-medium data-[outside-view]:text-black/30 data-[selected]:text-white data-[unavailable]:pointer-events-none data-[unavailable]:text-black/30 data-[unavailable]:line-through before:absolute before:top-[5px] before:hidden before:rounded-full before:size-1 before:bg-white data-[today]:text-blue-500 data-[today]:data-[selected]:text-white data-[today]:before:block data-[today]:before:bg-blue-500 data-[today]:before:-translate-y-[2px] data-[selected]:before:!bg-white"
+                    />
+                  </DatePickerClose>
                 </DatePickerCell>
               </DatePickerGridRow>
             </DatePickerGridBody>
           </DatePickerGrid>
         </div>
+
+        <DatePickerClose asChild>
+          <UIButton
+            variant="secondary"
+            size="s"
+            class="w-full"
+          >
+            Закрыть
+          </UIButton>
+        </DatePickerClose>
       </DatePickerCalendar>
     </DatePickerContent>
   </DatePickerRoot>
@@ -77,4 +89,5 @@
 
 <script lang="ts" setup>
 import Flex from '~/components/shared/Flex.vue'
+import UIButton from '~/components/shared/UIButton.vue'
 </script>
